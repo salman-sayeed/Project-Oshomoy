2df7c84b9edc5b5d4fc49582148a5e25f1e285ce
Sadikul Hub <sadikul821@gmail.com>
2024-09-24 21:05:42 +0600

    payment

--- Oshomoy/PaymentSelect.Designer.cs
+++ Oshomoy/PaymentSelect.Designer.cs
@@ -28,19 +28,195 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panelBkash = new System.Windows.Forms.Panel();
+            this.pNagad = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lBkash = new System.Windows.Forms.Label();
+            this.lNagad = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.btnPayment = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelBkash.SuspendLayout();
+            this.pNagad.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panelBkash
+            // 
+            this.panelBkash.BackColor = System.Drawing.Color.Gainsboro;
+            this.panelBkash.Controls.Add(this.btnPayment);
+            this.panelBkash.Controls.Add(this.pictureBox1);
+            this.panelBkash.Controls.Add(this.lBkash);
+            this.panelBkash.Location = new System.Drawing.Point(58, 120);
+            this.panelBkash.Name = "panelBkash";
+            this.panelBkash.Size = new System.Drawing.Size(255, 367);
+            this.panelBkash.TabIndex = 0;
+            // 
+            // pNagad
+            // 
+            this.pNagad.BackColor = System.Drawing.Color.Gainsboro;
+            this.pNagad.Controls.Add(this.button1);
+            this.pNagad.Controls.Add(this.pictureBox2);
+            this.pNagad.Controls.Add(this.lNagad);
+            this.pNagad.Location = new System.Drawing.Point(373, 120);
+            this.pNagad.Name = "pNagad";
+            this.pNagad.Size = new System.Drawing.Size(255, 367);
+            this.pNagad.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.pictureBox3);
+            this.panel2.Location = new System.Drawing.Point(701, 120);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(255, 367);
+            this.panel2.TabIndex = 2;
+            // 
+            // lBkash
+            // 
+            this.lBkash.AutoSize = true;
+            this.lBkash.BackColor = System.Drawing.Color.DarkCyan;
+            this.lBkash.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lBkash.Location = new System.Drawing.Point(76, 14);
+            this.lBkash.Name = "lBkash";
+            this.lBkash.Size = new System.Drawing.Size(67, 22);
+            this.lBkash.TabIndex = 3;
+            this.lBkash.Text = "Bkash";
+            // 
+            // lNagad
+            // 
+            this.lNagad.AutoSize = true;
+            this.lNagad.BackColor = System.Drawing.Color.DarkCyan;
+            this.lNagad.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lNagad.Location = new System.Drawing.Point(85, 14);
+            this.lNagad.Name = "lNagad";
+            this.lNagad.Size = new System.Drawing.Size(70, 22);
+            this.lNagad.TabIndex = 4;
+            this.lNagad.Text = "Nagad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.DarkCyan;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(99, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 22);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Bank";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(38, 52);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(166, 212);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(45, 52);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(170, 212);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(48, 52);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(174, 212);
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            // 
+            // btnPayment
+            // 
+            this.btnPayment.Location = new System.Drawing.Point(56, 285);
+            this.btnPayment.Name = "btnPayment";
+            this.btnPayment.Size = new System.Drawing.Size(119, 38);
+            this.btnPayment.TabIndex = 1;
+            this.btnPayment.Text = "Payment";
+            this.btnPayment.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(56, 285);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Payment";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(73, 285);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 38);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Payment";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Salmon;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(301, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(345, 33);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Select Payment Method";
             // 
             // PaymentSelect
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pNagad);
+            this.Controls.Add(this.panelBkash);
             this.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "PaymentSelect";
             this.Size = new System.Drawing.Size(1000, 500);
+            this.panelBkash.ResumeLayout(false);
+            this.panelBkash.PerformLayout();
+            this.pNagad.ResumeLayout(false);
+            this.pNagad.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelBkash;
+        private System.Windows.Forms.Panel pNagad;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lBkash;
+        private System.Windows.Forms.Label lNagad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnPayment;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Oshomoy/PaymentSelect.cs
+++ Oshomoy/PaymentSelect.cs
@@ -16,5 +16,10 @@ namespace Oshomoy
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
